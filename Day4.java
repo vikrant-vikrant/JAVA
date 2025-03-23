750a62660d8e4345cd1d85c12ee48d81b1ae7a21
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-03-23 11:13:40 +0530

    Know Operators and solve practice Q

---
+++ Day4.java
@@ -0,0 +1,46 @@
+public class Day4 {
+  public static void main(String args[]){
+  //Types of Operators
+  //Arithmetic Operators (Binary/Unary)
+  //Binary(+,-,*,/,%)
+  int a = 6;
+  int b = 80;
+  System.out.println("Add = " + (a+b));
+  System.out.println("Subtraction = " + (a-b));
+  System.out.println("Multiply = " + (a*b));
+  System.out.println("Divide = " + (b/a));
+  System.out.println("Module = " + (a%b));
+  //Unary(++,--)
+  // pre incriment (++a)
+  //1. value change 2.value use
+  int c = 10;
+  int d = ++c;
+  System.out.println(c);
+  System.out.println(d);
+  //pre Decrement (--a)
+  //1.value change 2.value use
+  int cc = 10;
+  int dd = --cc;
+  System.out.println(cc);
+  System.out.println(dd);
+  //post incriment(a++)
+  //1. value use 2.value change
+
+  int e = 15;
+  int f = e++;
+  System.out.println(e);
+  System.out.println(f);
+  //post Decrement (a--)
+  //1.value use 2.value change 
+  int ee = 15;
+  int ff = ee--;
+  System.out.println(ee);
+  System.out.println(ff);
+  // Relational Operators (==,!=,>,<,<=,>=)
+  // Logical Operators (&&,||,!)
+  // Bitwise Operators
+
+  // Assignment Operators (=, +=, -=, *=, /=)
+
+  }
+}
