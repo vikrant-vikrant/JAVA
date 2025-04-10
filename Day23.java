813e73c980389ff6edd31df2ef46cf5e3f019717
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2025-04-10 13:45:39 +0530

    Array, Linear Search & Largest and Smallest number

--- Day23.java
+++ Day23.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.*;
 public class Day23 {
     //SOLID RHOMBUS
     //SOLVED BY OWN
@@ -29,35 +31,93 @@ public class Day23 {
             System.out.println();
         }
     }
+
     //DIAMOND PATTERN
-    public static void diamond(int n){
-      //1st half
-      for(int i = 1;i<=n;i++){
-        for(int j =1;j<=n-i;j++){
-          System.out.print("  ");
+    public static void diamond(int n) {
+        //1st half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= ((i * 2) - 1); k++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
         }
-        for(int k=1;k<=((i*2)-1);k++){
-          System.out.print("* ");
-          
+        //2nd half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= ((i * 2) - 1); k++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
         }
-          System.out.println();
-      }
-      //2nd half
-      for(int i = n;i>=1;i--){
-        for(int j =1;j<=n-i;j++){
-          System.out.print("  ");
+    }
+
+    //Arrays as Function Arguments
+    public static void update(int marks[]) {
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 2;
         }
-        for(int k=1;k<=((i*2)-1);k++){
-          System.out.print("* ");
-          
+    }
+
+    //LINEAR SEARCH
+    public static int linearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
         }
-          System.out.println();
-      }
+        return -1;
+    }
+
+    //LARGEST & SMALLEST NUMBER
+    //SOLVED BY OWN
+    public static int largestNum(int num[]) {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] > largest) {
+                largest = num[i];
+            }
+            if (num[i] < smallest) {
+                smallest = num[i];
+            }
+        }
+        System.out.println("Smallest number : " + smallest);
+        return largest;
     }
 
     public static void main(String[] args) {
-    //ARRAYS
-    // int marks[] = new int[10];
+        int marks[] = {10, 20, 30, 40, 50, 40, 5, 32, 11};
+        int biggestNum = largestNum(marks);
+        System.out.println("Largest number : " + biggestNum);
+
+        int key = 40;
+        int index = linearSearch(marks, key);
+        if (index == -1) {
+            System.out.println("NOT found");
+        } else {
+            System.out.println("KEY is at  index : " + index);
+        }
+        // ARRAYS
+        update(marks);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+        System.out.println("maths " + marks[0]);
+        System.out.println("hindi " + marks[1]);
+        marks[2] = 100;
+        System.out.println("english " + marks[2]);
+        System.out.println("Length " + marks.length);
         // diamond(5);
         // hollow_rhombus(7);
         // solidRhombus(7);
