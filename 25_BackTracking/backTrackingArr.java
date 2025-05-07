a3c11dcfe969de2c526c69c394fa78e8e48cde2f
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-07 21:36:22 +0530

    find  subset

--- 25_BackTracking/backTrackingArr.java
+++ 25_BackTracking/backTrackingArr.java
@@ -10,15 +10,35 @@ public class backTrackingArr {
         changeArr(arr, i + 1, value + 1);
         arr[i] = arr[i] - 2;
     }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ,");
         }
     }
+
+    //find subasets
+    public static void findSubsets(String str, String ans, int i) {
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.print("null");
+            } else {
+                System.out.print(ans + ", ");
+            }
+            return;
+        }
+        //yes choice
+        findSubsets(str, ans + str.charAt(i), i + 1);
+        //no choice
+        findSubsets(str, ans, i + 1);
+    }
+
     public static void main(String[] args) {
-        int arr[] = new int[5];
-        changeArr(arr, 0, 1);
-        System.out.println();
-        printArr(arr);
+        String alpha = "abc";
+        findSubsets(alpha, "", 0);
+        // int arr[] = new int[5];
+        // changeArr(arr, 0, 1);
+        // System.out.println();
+        // printArr(arr);
     }
 }
