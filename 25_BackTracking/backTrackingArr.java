08adca2b95fd34279b35d34e0870a5ca34f31f14
sidharth <232006sidharth@email.com>
2025-05-07 09:19:07 +0530

    backTracking

---
+++ 25_BackTracking/backTrackingArr.java
@@ -0,0 +1,26 @@
+
+public class backTrackingArr {
+
+    public static void changeArr(int arr[], int i, int value) {
+        if (i == arr.length) {
+            printArr(arr);
+            return ;
+        }
+        arr[i] = value;
+        changeArr(arr, i + 1, value + 1);
+        arr[i] = arr[i] - 2;
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ,");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        changeArr(arr, 0, 1);
+        System.out.println();
+        printArr(arr);
+    }
+}
