0b6d458d77e42184b617dcefd6f2133f5bb25072
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-12 23:10:27 +0530

    LinkedList basic

---
+++ 28_LinkedList/Part_i.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+public class Part_i{
+  public static class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static Node head;
+  public static Node tail;
+
+  public static void main(String[] args) {
+    LinkedList ll = new LinkedList<>();
+    ll.head = new Node(1);
+    ll.head.next = new Node(2);
+  }
+
+}
