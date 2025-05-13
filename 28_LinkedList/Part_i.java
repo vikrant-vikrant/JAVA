ec7b3fc7f0c974a65cd6bfdde249d349fbaf0bca
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-13 23:29:25 +0530

    Add in MIddle & Remove in LL

--- 28_LinkedList/Part_i.java
+++ 28_LinkedList/Part_i.java
@@ -11,10 +11,12 @@ public class Part_i {
 
   public static Node head;
   public static Node tail;
+  public static int size;
 
   public void addFirst(int data){
     //create new node
     Node newNode = new Node(data);
+    size++;
     if(head == null){
       head = tail = newNode;
       return;
@@ -28,6 +30,7 @@ public class Part_i {
   public void addLast(int data){
     //create new node
     Node newNode = new Node(data);
+    size++;
     if(head == null){
       head = tail = newNode;
       return;
@@ -50,7 +53,53 @@ public class Part_i {
     }
     System.out.println("null ");
   }
+  //Add in the Middle of LL
+  public void add(int idx, int data){
+    if(idx == 0){
+      addFirst(data);
+      return;
+    }
+    Node newNode = new Node(data);
+    size++;
+    Node temp = head;
+    int i=0;
 
+    while(i < idx-1){
+      temp = temp.next;
+      i++;
+    }
+    newNode.next = temp.next;
+    temp.next = newNode;
+  }
+  //Remove in a LL
+  public int removeFrist(){
+    if(size == 0){
+      System.out.pirntln("LL is empty");
+      return Integer.MAX_VALUE;
+    }else if(size == 1){
+      int val = head.data;
+      head = tail = null;
+      return val;
+    }
+    int val= head.data;
+    head = head.next;
+    return val;
+  }
+  public int removeLast(){
+    if(size == 0){
+      System.out.print("LL id empty");
+      return Integer.MIN_VALUE;
+
+    }else if(size  == 1){
+      int val = head.data;
+      head = tail = null;
+      size = 0;
+      return ;
+    }
+
+    //prev : i = size
+    
+  }
   public static void main(String[] args) {
     Part_i ll = new Part_i();
     ll.addFirst(2);
@@ -60,6 +109,7 @@ public class Part_i {
     ll.addLast(3);
     ll.print();
     ll.addLast(4);
+    ll.add(2,9);
     ll.print();
     
   }
