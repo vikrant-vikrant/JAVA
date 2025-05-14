c971e3523adfd079f813d06f8b7d213285a5b83d
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-14 09:09:58 +0530

    Itaerative search & recision search

--- 28_LinkedList/Part_i.java
+++ 28_LinkedList/Part_i.java
@@ -74,7 +74,7 @@ public class Part_i {
   //Remove in a LL
   public int removeFrist(){
     if(size == 0){
-      System.out.pirntln("LL is empty");
+      System.out.println("LL is empty");
       return Integer.MAX_VALUE;
     }else if(size == 1){
       int val = head.data;
@@ -94,11 +94,66 @@ public class Part_i {
       int val = head.data;
       head = tail = null;
       size = 0;
-      return ;
+      return val;
     }
 
-    //prev : i = size
-    
+    //prev : i = size - 2
+    Node prev = head;
+    for(int i=0;i<size-2;i++){
+      prev = prev.next;
+    }
+
+    int val = tail.data;//tail data
+    prev.next = null;
+    tail = prev;
+    size--;
+    return val;    
+  }
+  //itaerative search
+  public int itrSearch(int key){
+    Node temp = head;
+    int i = 0;
+
+    while(temp != null){
+      if(temp.data == key){
+        return i;
+      }
+      temp = temp.next;
+      i++;
+    }
+
+    return -1;
+  }
+
+  public int helper(Node head, int key){
+    if(head == null){
+      return -1;
+    }
+    if(head.data == key){
+      return 0;
+    }
+    int idx = helper(head.next, key);
+    if(idx == -1){
+      return -1;
+    }
+    return idx+1;
+  }
+  public int recSearch(int key){
+    return helper(head,key);
+  }
+
+  public int reverse(){
+    Node prev = null;
+    Node curr = tail = head;
+    Node next;
+
+    while(curr != null){
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    head = prev;
   }
   public static void main(String[] args) {
     Part_i ll = new Part_i();
@@ -110,6 +165,15 @@ public class Part_i {
     ll.print();
     ll.addLast(4);
     ll.add(2,9);
+    ll.removeFrist();
+    ll.print();
+
+    ll.removeLast();
+    ll.print();
+    System.out.println(ll.size);
+    System.out.println(ll.recSearch(3));
+    System.out.println(ll.recSearch(9));
+    ll.reverse();
     ll.print();
     
   }
