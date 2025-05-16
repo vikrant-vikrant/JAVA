241e23c6804046400eb4e156308fa437be5177d7
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-16 22:58:24 +0530

    push at bottem of stack & reverse a  string using stack

--- 29_Stacks/StackCollFramework.java
+++ 29_Stacks/StackCollFramework.java
@@ -1,16 +1,45 @@
 import java.util.*;
 
 public class StackCollFramework{
+  //Push at the Bottom of the Stack
+  public static void pushAtBottom(Stack<Integer> s, int data){
+    if(s.isEmpty()){
+      s.push(data);
+      return;
+    }
+    int top = s.pop();
+    pushAtBottom(s,data);
+    s.push(top);
+  }
+  //Reverse a String using a Stack
+  public static String reverseString(String str){
+    Stack<Character> s = new Stack<>();
+    int idx = 0;
+    while(idx < str.length()){
+      s.push(str.charAt(idx));
+      idx++;
+    }
+    StringBuilder result = new StringBuilder("");
+    while(!s.isEmpty()){
+      char curr = s.pop();
+      result.append(curr);
+    }
+
+    return result.toString();
+  }
   public static void main(String args[]){
     // Stack s = new Stack();
-    Stack<Integer> s = new Stack<>();
-    s.push(1);
-    s.push(2);
-    s.push(3);
-
-    while(!s.isEmpty()){
-      System.out.println(s.peek());
-      s.pop();
-    }
+    String str = "HelloWorld";
+    System.out.println(reverseString(str));
+    // Stack<Integer> s = new Stack<>();
+    // s.push(1);
+    // s.push(2);
+    // s.push(3);
+    // pushAtBottom(s, 4);
+    // while(!s.isEmpty()){
+    //   System.out.println(s.pop());
+    //   // System.out.println(s.peek());
+    //   s.pop();
+    // }
   }
 }
