0dbc22486c4e31b32207c9960113b37ad84bf2e6
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-16 10:49:20 +0530

    Use Doubly LL & Zigzag

--- 28_LinkedList/Part_i.java
+++ 28_LinkedList/Part_i.java
@@ -326,6 +326,44 @@ public class Part_i {
     //merge
     return merge(newLeft,newRight);
   }
+
+  //ZigZag
+  public void zigzag(){
+    //find mid
+    Node slow = head;
+    Node fast = head.next;
+    while(fast != null && fast.next != null){
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    Node mid = slow;
+    //reverse 2nd half
+    Node curr = mid.next;
+    mid.next = null;
+    Node prev = null;
+    Node next;  
+
+    while(curr != null){
+      next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    Node left = head;
+    Node right = prev;
+    Node nextL, nextR;
+    //merge 
+    while(left != null && right != null){
+      nextL = left.next;
+      left.next = right;
+      nextR = right.next;
+      right.next = nextL;
+
+      left = nextL;
+      right = nextR;
+    }
+  }
+
   public static void main(String[] args) {
     Part_i ll = new Part_i();
     ll.addFirst(1);
@@ -334,7 +372,7 @@ public class Part_i {
     ll.addFirst(4);
     ll.addFirst(5);
     ll.print();
-    ll.head = ll.mergeSort(ll.head);
+    ll.zigzag();
     ll.print();
   }
 }
