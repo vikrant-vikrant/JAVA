31458b08604357fea01f14c4303dfb870c5db07b
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-05-19 23:29:05 +0530

    duplicate parentheses

--- 29_Stacks/questions.java
+++ 29_Stacks/questions.java
@@ -29,8 +29,30 @@ public class questions{
       return false;
     }
   }
+  //Duplicate Parentheses
+  public static boolean isDuplicate(String str) {
+      Stack<Character> s = new Stack<>();
+
+      for(int i=0;i<str.length();i++){
+        char ch = str.charAt(i);
+        //closing
+        if(ch == ')'){
+          int count = 0;
+          while(s.pop() != '('){
+            count++;
+          }
+          if(count < 1){
+            return true;
+          }
+        }else{
+          s.push(ch);
+        }
+      }
+      return false;
+  }
   public static void main(String args[]){
-    String str = "({}[])";
-    System.out.println(isValid(str));
+    String str = "(a+((b)))";
+    // String str2 = "(a+(b))";
+    System.out.println(isDuplicate(str));
   }
 }
