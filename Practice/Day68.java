2686e84fd120701e8f4b18075eef1ad46a73f894
vikrant-vikrant <vikrant2003vikrant@gmail.com>
2025-06-12 20:43:39 +0530

    get sortest path question

--- Practice/Day68.java
+++ Practice/Day68.java
@@ -11,9 +11,27 @@ public class Day68 {
         return true;
     }
 
+    public static float getPath(String path){
+        int x=0,y=0;
+        for(int i=0;i<path.length();i++){
+            char dir = path.charAt(i);
+
+            switch (dir) {
+                case 'S' -> y--;
+                case 'N' -> y++;
+                case 'E' -> x++;
+                default -> x--;
+            }
+        }
+        return (float)Math.sqrt((x*x) + (y*y));
+    }
     public static void main(String[] args) {
-        String x = "racecar";
-        System.out.println(isPalindrome(x));
+
+        // String path = "WNEENESENNN";
+        // System.out.println(getPath(path));
+        String word = "This is a, Cat";
+        // String x = "racecar";
+        // System.out.println(isPalindrome(x));
         // char arr[] = {'a','b','c'};
         // String str = "koma l"; 
         // String str2 = new String("komal");
